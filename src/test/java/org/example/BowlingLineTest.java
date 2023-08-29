9491177778956a3cd64dd057b396be20f0f2ff29
Adrian Yates <adeyatz@gmail.com>
2023-08-29 18:24:10 +0100

    All standardFrame test pass, no Frames classes yet

--- src/test/java/org/example/BowlingLineTest.java
+++ src/test/java/org/example/BowlingLineTest.java
@@ -22,4 +22,53 @@ public class BowlingLineTest {
 
         assertEquals (0, bowler.sum());
     }
+
+    @Test
+    public void checkSingleScoringRolls() {
+
+        BowlingLine bowler = new BowlingLine (BOWLER_NAME);
+        bowler.addStandardFrame (1, 2);
+
+        assertEquals (3, bowler.sum());
+    }
+
+    @Test
+    public void checkTemScoringRolls() {
+        int i;
+
+        BowlingLine bowler = new BowlingLine (BOWLER_NAME);
+        for (i = 0; i < 10; i++) {
+            bowler.addStandardFrame(1, 2);
+        }
+
+        assertEquals (30, bowler.sum());
+    }
+
+
+    @Test
+    public void checkTemScoringRollsOf5() {
+        int i;
+
+        BowlingLine bowler = new BowlingLine (BOWLER_NAME);
+        for (i = 0; i < 10; i++) {
+            bowler.addStandardFrame(3, 2);
+        }
+
+        assertEquals (50, bowler.sum());
+    }
+
+    @Test
+    public void checkTemScoringRollsOf9() {
+        int i;
+
+        BowlingLine bowler = new BowlingLine (BOWLER_NAME);
+        for (i = 0; i < 10; i++) {
+            bowler.addStandardFrame(9, 0);
+        }
+
+        assertEquals (90, bowler.sum());
+    }
+
+
+
 }
